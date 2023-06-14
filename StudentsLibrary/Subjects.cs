aba0a8a9b650193b760124d1e6974845c1a0f885
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2023-06-14 16:59:33 +0530

    Update Subjects.cs

--- StudentsLibrary/Subjects.cs
+++ StudentsLibrary/Subjects.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;   
 using System.Linq;
 using System.Text;
@@ -13,13 +13,77 @@ namespace StudentsLibrary
         string _SubjectName;
         string _TeacherName;
         string _BookName;
+        int _specializationType;
+        string _UnivName;
+
+        static Subjects()
+        {
+            Console.WriteLine("Initialize");
+
+        }
+        //Default Constructor
+        public Subjects() { }
+
+
+      
+
+        public Subjects(int spType,string UnivName):this(UnivName)
+        {
+           _specializationType = spType;   
+        }
+        public Subjects(string UnivName)
+        {
+            _UnivName   = UnivName;
+        }
+
+
+        public Subjects(string subname, int spType, string bname, string UnivName) : this(spType, UnivName)
+        {
+            _SubjectName = subname;
+            _BookName = bname;
+        }
+
+        public Subjects(Subjects copy)
+        {
+            this._SubjectName = copy._SubjectName;
+            this._BookName = copy._BookName;
+            this._specializationType = copy._specializationType;
+            this._UnivName = copy._UnivName;
+        }
+
+
+
+
+
+        public Subjects(string subname,string tname,string bname,int spType):this(subname,tname,bname)
+        {
+            
+            _specializationType = spType;
+        }
+        public Subjects(string subname, string tname, string bname)
+        {
+            _SubjectName = subname;
+            _TeacherName = tname;
+            _BookName = bname;
+
+        }
+
+
+
+
+
+
+
+
+
 
         public void DisplaySubjectDetails()
         {
             Console.WriteLine(_SubjectName);
-            Console.WriteLine(_TeacherName);
+           // Console.WriteLine(_TeacherName);
+            Console.WriteLine(_specializationType);
             Console.WriteLine(_BookName);
-
+            Console.WriteLine( _UnivName);
 
         }
 
