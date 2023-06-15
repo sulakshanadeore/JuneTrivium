7b542803cb82fef8c13695ca97bf642bd2042376
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2023-06-15 16:19:07 +0530

    Add files via upload

---
+++ JaggedWithMutliDimensional.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysDemo
+{
+    public class JaggedWithMutliDimensional
+    {
+        static void Main(string[] args)
+        {
+            int[][,] multijagged = new int[4][,] 
+            { 
+            new int[,] { { 1, 2 },{3,4},{5,6 } 
+            },
+            new int[,] {{ 1, 2 },{3,4},{5,6 }, { 5,7 }
+
+            },
+            new int[,] {{ 1, 2 },{3,4}
+            },
+            new int[,] {{ 1, 2 },{3,4},{5,6 }, { 5,7 },{6,7 },{7,8 } }
+            };
+
+
+            for (int i = 0; i < multijagged.Length; i++)
+            {
+                int x = 0;
+                for (int ele = 0; ele < multijagged[i].GetLength(x); ele++)
+                {
+                    for (int k = 0; k < multijagged.Rank; k++)
+                    {
+                        Console.Write(multijagged[i][ele,k] + "\t");
+                        Console.WriteLine();
+                    }
+                  
+                   
+                }
+                  x++;
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
