a949524ac23d5d83949f324c58fbde57f80afa4b
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2023-06-15 15:37:49 +0530

    Add files via upload

---
+++ EmployeeArrayDemo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysDemo
+{
+    public class EmployeeArrayDemo
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[5];
+
+            employees[0]=new Employee();
+            employees[0].Empid = 1;
+            employees[0].EmpName = "Mina";
+            employees[0].EmpCity = "Hyderabad";
+            employees[0].BasicSal = 10000;
+
+
+
+            employees[1]=new Employee();
+
+            employees[2]=new Employee();
+
+            employees[3]=new Employee();
+
+            employees[4]=new Employee();
+
+
+            foreach (var e in employees) 
+            {
+                Console.WriteLine(e.Empid);
+                Console.WriteLine(e.EmpName);
+                Console.WriteLine(e.EmpCity);
+                Console.WriteLine(e.BasicSal);
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+            
+
+            Array a=Array.CreateInstance(typeof(int), 10);
+            int[] valuesArray = (int[])a;
+            valuesArray[0] = 1;
+            valuesArray[1] = 2;
+            valuesArray[2] = 3;
+            valuesArray[3] = 4;
+            valuesArray[4] = 5;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+
+            }
+
+
+           foreach (var item in valuesArray)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            int[] arr2 = new int[10] { 100, 3452, 12, 35, 67, 81, 1, 23, 4, 89 };
+
+            Array.Sort(arr2);
+            foreach (var item in arr2)
+            {
+                Console.WriteLine(item);
+            }
+            //Array.Reverse(arr2);
+            //Console.WriteLine("Printing reverse sorted Array");
+            //foreach (var item in arr2)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Console.WriteLine("-------------");
+            //Array.Clear(arr2,0,arr2.Length);
+            //Console.WriteLine("Printing cleared  Array");
+            //foreach (var item in arr2)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            Console.WriteLine("--------------------------");
+            int ind=Array.BinarySearch(arr2,100);
+            Console.WriteLine( ind);
+            Array.Copy(arr2, valuesArray, 10);
+            Console.WriteLine("After copy");
+            foreach (var item in valuesArray)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
