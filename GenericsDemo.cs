0a5a982724d983f018a61c647052ae0942db28c6
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2023-06-19 12:55:35 +0530

    Generics--Dictionary,SortedList,SortedSet

--- GenericsDemo.cs
+++ GenericsDemo.cs
@@ -15,6 +15,85 @@ namespace CollectionsDemo
             //WorkingWithListofIntegers();
             //Console.WriteLine("----------------------------------");
 
+            //WorkingwithComplexTypeUsingList();
+
+            // WorkingWithKeyValuePairs();
+
+            //WorkingWithSortedSet();
+
+            Console.ReadLine();
+        }
+
+        private static void WorkingWithSortedSet()
+        {
+            SortedSet<Student> students = new SortedSet<Student>();
+            students.Add(new Student() { Rollno = 1, Name = "ankita", Age = 12 });
+            students.Add(new Student() { Rollno = 100, Name = "Ana", Age = 12 });
+            students.Add(new Student() { Rollno = 2, Name = "Amita", Age = 12 });
+            students.Add(new Student() { Rollno = 99, Name = "anandita", Age = 12 });
+
+            foreach (var item in students)
+            {
+                Console.WriteLine(item.Rollno + "  " + item.Name + "  " + item.Age);
+
+            }
+        }
+
+        private static void WorkingWithKeyValuePairs()
+        {
+            //Dictionary<int, string> dic = new Dictionary<int, string>();
+            //dic.Add(100, "Jack");
+            //dic.Add(10, "Sita");
+            //dic.Add(1, "Mac");
+            //dic.Add(101, "Jill");
+            //dic.Add(99, "Jim");
+
+            //Dictionary<int, string>.Enumerator t = dic.GetEnumerator();
+            //while (t.MoveNext())
+            //{
+            //    Console.WriteLine(t.Current.Key + " " + t.Current.Value);
+
+
+            //}
+
+            //Console.WriteLine("------using foreach loop-----");
+            //foreach (var item in dic)
+            //{
+            //    Console.WriteLine(item.Key + " " + item.Value);
+
+            //}
+
+
+
+            //SortedDictionary<int, string> dic = new SortedDictionary<int, string>();
+            // dic.Add(100, "Jack");
+            // dic.Add(10, "Sita");
+            // dic.Add(1, "Mac");
+            // dic.Add(101, "Jill");
+            // dic.Add(99, "Jim");
+
+            // SortedDictionary<int, string>.Enumerator t = dic.GetEnumerator();
+            // while (t.MoveNext())
+            // {
+            //     Console.WriteLine(t.Current.Key + " " + t.Current.Value);
+
+
+            // }
+
+            // Console.WriteLine("------using foreach loop-----");
+            // foreach (var item in dic)
+            // {
+            //     Console.WriteLine(item.Key + " " + item.Value);
+
+            // }
+
+
+
+           
+        }
+
+        private static void WorkingwithComplexTypeUsingList()
+        {
             List<Student> students = new List<Student>() {
             new Student() {Rollno=6,Name="Jack",Age=20},
             new Student(){Rollno=7,Name="Jim",Age=20}
@@ -27,7 +106,7 @@ namespace CollectionsDemo
 
 
             students.Add(s);
-            students.Add(new Student() { Rollno=2,Name="Nikita",Age=21 });
+            students.Add(new Student() { Rollno = 2, Name = "Nikita", Age = 21 });
             students.Add(new Student() { Rollno = 4, Name = "Nikita", Age = 22 });
             students.Add(new Student() { Rollno = 3, Name = "Kamal", Age = 19 });
             students.Add(new Student() { Rollno = 5, Name = "Sima", Age = 20 });
@@ -36,39 +115,39 @@ namespace CollectionsDemo
 
             foreach (var item in students)
             {
-                Console.WriteLine(item.Rollno + " " + item.Name  + "   "+ item.Age);
+                Console.WriteLine(item.Rollno + " " + item.Name + "   " + item.Age);
             }
 
-           //Student studfound =students.Find(s1 => s1.Name == "Sima");
-           // if (studfound!=null)
-           // {
-           //     Console.WriteLine(studfound.Rollno);
-           //     Console.WriteLine(studfound.Name);
-           //     Console.WriteLine( studfound.Age);
+            //Student studfound =students.Find(s1 => s1.Name == "Sima");
+            // if (studfound!=null)
+            // {
+            //     Console.WriteLine(studfound.Rollno);
+            //     Console.WriteLine(studfound.Name);
+            //     Console.WriteLine( studfound.Age);
 
-           // }
+            // }
 
-           // Console.WriteLine("-------Age Output-----");
-
-
-           // studfound =students.Find(a => a.Age == 20);
-           // if (studfound != null)
-           // {
-
-           //     Console.WriteLine(studfound.Rollno);
-           //     Console.WriteLine(studfound.Name);
-           //     Console.WriteLine(studfound.Age);
-           // }
-
-           // List<Student> studlist=students.FindAll(a=>a.Age==20);
-           // studlist.ForEach(s1 => 
-           // { 
-           //     Console.WriteLine("Student details are : ");
-           //     Console.WriteLine(s1.Rollno + " " + s1.Name + " " + s1.Age);
+            // Console.WriteLine("-------Age Output-----");
 
 
-           // }
-           // );
+            // studfound =students.Find(a => a.Age == 20);
+            // if (studfound != null)
+            // {
+
+            //     Console.WriteLine(studfound.Rollno);
+            //     Console.WriteLine(studfound.Name);
+            //     Console.WriteLine(studfound.Age);
+            // }
+
+            // List<Student> studlist=students.FindAll(a=>a.Age==20);
+            // studlist.ForEach(s1 => 
+            // { 
+            //     Console.WriteLine("Student details are : ");
+            //     Console.WriteLine(s1.Rollno + " " + s1.Name + " " + s1.Age);
+
+
+            // }
+            // );
 
             students.Sort();
             Console.WriteLine("After sorting on Rollno using IComparer ");
@@ -77,24 +156,14 @@ namespace CollectionsDemo
                 Console.WriteLine(item.Rollno + " " + item.Name + "   " + item.Age);
             }
 
+            Console.WriteLine("sorting by name");
+            SortByStudentName sn = new SortByStudentName();
+            students.Sort(sn);
+            foreach (var item in students)
+            {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            Console.ReadLine();
+                Console.WriteLine(item.Rollno + " " + item.Name + "   " + item.Age);
+            }
         }
 
         private static void WorkingWithListofIntegers()
