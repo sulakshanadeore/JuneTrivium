0b131f4ae34b2dbc7f2c3769108d1a1e25d7ea4f
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2023-06-20 15:12:17 +0530

    Add files via upload
    linq demo--app

---
+++ ObjectLinqDemo.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace LinqDemo
+{
+    internal class ObjectLinqDemo
+    {
+        static void Main(string[] args)
+        {
+            //int[] arr = new int[10] {3,32,12,78,2,90,87,56,44,0 };
+            //2 ways
+            //Query Syntax
+            //Method Syntax
+
+            //var evenNos = from a in arr
+            //              where a % 2 == 0
+            //              select a;
+            //var greaterthan50=from a in arr
+            //                  where a>50 || a<=100
+            //                  orderby a
+            //                  select a;
+            //arr[9] = 100;
+
+            //foreach (int i in greaterthan50)
+            //{
+            //    Console.WriteLine(i);
+
+            //}
+            //Console.WriteLine("--------------");
+            //List<string> stringList = new List<string>() {
+            //"Hello",
+            //"World",
+            //"Bye",
+            //"GoodMorning",
+            //"GoodDay"
+                      
+            //};
+
+            //var result = from s in stringList
+            //             where s.Contains("Hello")
+            //             select s;
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            List<Student> students = new List<Student>() {
+            new Student{Studid=11,Name="Asha",Age=12,StudStd=10 },
+            new Student {Studid=3,Name="Priya",Age=20,StudStd=9  },
+            new Student {Studid=2,Name="Kajal",Age=12,StudStd=9  },
+            new Student {Studid=4,Name="Nisha",Age=12 ,StudStd=10 },
+            new Student{Studid=1,Name="Usha",Age=12,StudStd=10  }
+            
+            };
+            //var data = from s in students
+            //           select s;
+
+            //var data = from s in students
+            //           where s.Age == 12
+            //           orderby s.Studid ascending, s.Name descending
+
+            //           select new { s.Studid, s.Name };
+            // select new {sname=s.Name,rollno=s.Studid };
+
+
+
+            //foreach (var item in data)
+            //{
+            //    //Console.WriteLine($"{item.Studid} \t {item.Name} \t {item.Age}");
+            //    // Console.WriteLine($"{item.rollno} \t {item.sname}"  );
+            //    Console.WriteLine($"{item.Studid} \t {item.Name}");
+
+            //    Console.WriteLine();
+            //}
+
+
+            var data = from s in students
+                       group s by s.StudStd into StdData
+                       select StdData;
+
+            foreach (var item in data)
+            {
+                Console.WriteLine("standard " + item.Key);
+                foreach (var studData in item)
+                {
+
+                    Console.WriteLine($"{studData.Studid} \t {studData.Name} \t {studData.Age}");
+                }
+            }      
+
+            
+
+
+
+            Console.ReadLine();
+                
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,108 +1,39 @@
-﻿using FeaturesDemo;
-using System.Net.Http.Headers;
+﻿using ClassLibrary1;
+using System.Runtime.CompilerServices;
 
-internal class Program
+
+
+//1) Top class should be a static class and a NonGeneric class
+//2) An extension method with same name and signature as an instance will not be called
+//3) They cannot be used for overriding existing methods,
+//4) It cannot work with properties,events  and fields
+//5) Extension methods must be static
+internal static class Program
 {
+    public static void MyExtensionMethod(this Class1 class1)
+    {
+        //Write a extension to my class functonality---Class1
+        Console.WriteLine("This is a extended method");
+    }
+    public static int IntegerConversionFromString(this string s)
+    { 
+    return Int32.Parse(s);
+    }
+
+
     private static void Main(string[] args)
     {
-        //Console.WriteLine("Hello, World!");
-        //Employee emp = new Employee() {Empid=1,Empname="Sakshi" };
-        //emp.AddEmployeeDetails(name: "Pradnya", empid: 34, Mobile: "24242423423",isIntern:true);
+        Console.WriteLine("Hello, World!");
 
-        //int a=20,b=30;
-        //Result(a, b, out int _, out int _);
-        ////Console.WriteLine(mult);
-
-        //Console.WriteLine(  "---------------");
-        //PrintObject( emp );
-        //Product p1 = new Product() {Proid=45,Prodname="Laptop",Price=60000 };
-        //PrintObject( p1 );
-        //Customer c1 = new Customer() {Custid=111,Name="sfsf" };
-        //PrintObject ( c1 ); 
-
-        //var bigvalue = 244242421142141;
-        //var valueSplit = 244_242421_1421_41;
-        //Console.WriteLine($"0:{bigvalue}:,1:{valueSplit}");
+        Class1 ob = new Class1();
+        Console.WriteLine(ob.Display());
+        Console.WriteLine(ob.Print());
+        ob.MyExtensionMethod();
 
 
-        Tuple<int, string, string> personData = new Tuple<int, string, string>(1, "Jack", "Jim");
-        var p = Tuple.Create(1, "Harish", "MJ", "Pune", "MH", "India", 111111, "Hello");
-
-        Console.WriteLine(personData.Item1);
-        Console.WriteLine(personData.Item2);
-        Console.WriteLine(personData.Item3);
-        Console.WriteLine("-----------");
-
-
-        Console.WriteLine(p.Item1);
-        Console.WriteLine(p.Item2);
-        Console.WriteLine(p.Item3);
-        Console.WriteLine(p.Item4);
-        Console.WriteLine(p.Item5);
-        Console.WriteLine(p.Item6);
-        Console.WriteLine(p.Item7);
-        Console.WriteLine(p.Rest);
-        Console.WriteLine(p.Rest.Item1);
-
-
-        var d1 = Tuple.Create(1, 2, Tuple.Create(3, 4, 5, 6, 7, 8), 9, 10, 11, 12, 13);
-        Console.WriteLine(d1.Item1);//1
-        Console.WriteLine(d1.Item2);//2
-        Console.WriteLine(d1.Item3); //3, 4, 5, 6, 7, 8
-        Console.WriteLine(d1.Item3.Item1); //3
-        Console.WriteLine(d1.Item4); //9
-        Console.WriteLine(d1.Rest.Item1); //13
-
-
-
-
-
-
-
-
-
+        string str = "1234788";
+        int i=str.IntegerConversionFromString();
+        Console.WriteLine(i);
+        Console.ReadLine();
     }
-
-
-    static void PrintObject(dynamic obj)
-    {
-        //if (obj is Employee)
-        //{
-        //    var c = obj as Employee;
-        //    Console.WriteLine($"Employee Details : {c.Empid} {c.Empname}");
-        //}
-        //else if (obj is Product)
-        //{
-        //    var p = obj as Product;
-        //    Console.WriteLine($"{p.Proid} {p.Prodname} {p.Price}");
-        //}
-        //else
-        //{
-        //    Console.WriteLine("Unknown object");
-        //}
-
-        switch (obj)
-        {
-            case Employee c:
-                    Console.WriteLine($"Employee Details : {c.Empid} {c.Empname}");
-                break;
-            case Product p:
-                Console.WriteLine($"{p.Proid} {p.Prodname} {p.Price}");
-                break;
-            default:
-                Console.WriteLine("Unknown object");
-                break;
-        }
-
-
-
-    }
-    static void Result(int i, int j, out int add, out int mult)
-    {
-        add = i + j;
-        mult = i * j;
-    
-    }
-
-
 }
