c0e5cefe5cbb57d863109f8e23b846e9f429af23
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2023-06-20 11:29:18 +0530

    Add files via upload

---
+++ IndexerDemo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeaturesDemo
+{
+    class Names
+    {
+
+        string[] listOfNames = new string[10];
+        public Names()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                listOfNames[i] = "No Name";
+
+            }
+        }
+
+        //Indexer is written with the name "this" to a property
+
+
+       
+
+        public string this[int index]
+        {
+            get { return listOfNames[index]; }
+            set { listOfNames[index] = value; }
+        }
+
+
+    }
+    internal class IndexerDemo
+    {
+        static void Main(string[] args)
+        {
+            Names n=new Names();
+            n[0] = "Jack";
+            n[1] = "Jim";
+            n[2] = "Ana";
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(n[i]);
+            }
+        }
+    }
+}
---
+++ StringBuilderDemo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeaturesDemo
+{
+    internal class StringBuilderDemo
+    {
+        static void Main(string[] args)
+        {
+            //string is immutable, if u create a string object and modify it, it always creates a new object.
+            string s = "Hello World";
+            s += "Good morning";
+
+
+
+            //StringBuilder is mutable, if u create stringbuilder object and perform any operations like append etc
+            //it works with the same instance of the stringbuilder object created.
+
+            StringBuilder sb=new StringBuilder();
+            sb.Append("Hello World");
+            sb.Append("Good day");
+
+        }
+    }
+}
