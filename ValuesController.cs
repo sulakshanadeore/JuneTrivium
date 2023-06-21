7dfdaf4bf56e87a84b028c5741047db2a63cc5d8
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2023-06-21 16:50:00 +0530

    ApiController
    Api controller Simple

---
+++ ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstWebAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+
+        static readonly List<string> str = new List<string> { "India", "America", "England" };
+        
+        [HttpGet(Name="GetValuesData")]
+        public List<string> Get()
+        {
+            return str;
+        }
+
+        [HttpPost(Name = "PostValuesData")]
+        public string Post([FromBody] string s)
+        {
+            str.Add(s);
+            return s;
+        }
+
+
+        [HttpPut(Name = "PutValuesData")]
+        public string Put( [FromBody] string oldstring,string toreplace)
+        {
+            int found = str.FindIndex(0,s1 => s1 == oldstring);
+            str.Insert(found, toreplace);
+
+            return toreplace;
+
+        }
+
+        [HttpDelete(Name = "DeleteValuesData")]
+
+        public string Delete([FromBody]string s)
+        {
+            string deleted = str.Find(s1 => s1 == s);
+            str.Remove(deleted);
+            return deleted;
+
+        }
+
+
+
+
+    }
+}
